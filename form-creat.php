7d393b96632a4127a06d19e2113276a6722832e8
AlyaSelviaTamzila <42310789+AlyaSelviaTamzila@users.noreply.github.com>
2019-03-13 12:39:43 +0700

    Add files via upload

---
+++ form-creat.php
@@ -0,0 +1,10 @@
+<h3>Tambah Data Dosen</h3>
+ 
+<form action="creat.php" method="post">
+	Nama dosen : <input type="text" name="nama_dosen"> <br>
+    <br>
+    No.Telepon : <input type="text" name="telp"> <br>
+    <br>
+	<input type="submit" name="Simpan" value="Simpan">
+</form>
+ 
